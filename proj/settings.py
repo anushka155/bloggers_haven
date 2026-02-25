e84cf154f659d28c897ed81194baa1223674a1ab
anushka155 <annggrg@gmail.com>
2026-02-25 15:21:40 +0545

    allowed hosts

--- proj/settings.py
+++ proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ["bloggers-haven-jt75.onrender.com"]
 
 
 # Application definition
